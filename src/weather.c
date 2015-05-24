f96f13b5c7992f4e06d2ade3c7175382a9bfc9fd
Mark Bush <mark.bush@ocado.com>
2015-05-24 22:56:04 +0100

    Added temperature scale

--- src/weather.c
+++ src/weather.c
@@ -8,17 +8,52 @@ void update_weather_temperature_display();
 void update_weather();
 
 uint32_t s_weather_state = RESOURCE_ID_IMAGE_QUERY;
-static BitmapLayer *s_conditions_layer;
 static TextLayer *s_temperature_layer;
-static GBitmap *s_conditions_bitmap;
 int s_temperature = 0;
 uint32_t s_temperature_unit = CELSIUS;
 int s_temperature_showing = 0;
 int s_weather_showing = 0;
 
-void setup_weather(Layer *root) {
-  // Create temperature Layer
 #ifdef PBL_COLOR
+static Layer *s_temperature_scale;
+static void temp_scale_update_proc(Layer *layer, GContext *ctx);
+int16_t temp_width[] = {
+  69, 69, 63, 56, 52, 49, 46, 42, 42, 39, 34, 34, 34, 33, 31, 29,
+  28, 27, 26, 24, 24, 23, 22, 21, 20, 18, 18, 17, 16, 15, 14, 14,
+  13, 13, 12,  9,  9,  9,  9,  9,  8,  8 , 8,  6,  6,  6,  6,  5,
+   5,  5,  4,  4,  4,  3,  3,  3,  3,  2,  2,  2,  2,  2,  2,  2,
+   1,  1,  1,  1,  1,  1,  0,  0,  0,  0,  1,  1,  1,  1,  1,  1,
+   2,  2,  2,  2,  2,  2,  2,  3,  3,  3,  3,  4,  4,  4,  5,  5,
+   5,  6,  6,  6,  6,  8,  8,  8,  9,  9,  9,  9,  9, 12, 13, 13,
+  14, 14, 15, 16, 17, 18, 18, 20, 21, 22, 23, 24, 24, 26, 27, 28,
+  29, 31, 33, 34, 34, 34, 39, 42, 42, 46, 49, 52, 56, 63, 69, 69
+};
+GColor temp_colour[13];
+#else
+static BitmapLayer *s_conditions_layer;
+static GBitmap *s_conditions_bitmap;
+#endif
+
+void setup_weather(Layer *root) {
+  // Create temperature Layers
+#ifdef PBL_COLOR
+  s_temperature_scale = layer_create(GRect(0, 0, 70, 144));
+  layer_add_child(root, s_temperature_scale);
+  layer_set_update_proc(s_temperature_scale, temp_scale_update_proc);
+  temp_colour[0] = GColorWhite;
+  temp_colour[1] = GColorCeleste;
+  temp_colour[2] = GColorElectricBlue;
+  temp_colour[3] = GColorCyan;
+  temp_colour[4] = GColorVividCerulean;
+  temp_colour[5] = GColorBlueMoon;
+  temp_colour[6] = GColorBlue;
+  temp_colour[7] = GColorElectricUltramarine;
+  temp_colour[8] = GColorVividViolet;
+  temp_colour[9] = GColorMagenta;
+  temp_colour[10] = GColorFashionMagenta;
+  temp_colour[11] = GColorFolly;
+  temp_colour[12] = GColorRed;
+
   s_temperature_layer = text_layer_create(GRect(2, 0, 26, 10));
   text_layer_set_font(s_temperature_layer, fonts_get_system_font(FONT_KEY_GOTHIC_09));
   text_layer_set_text_color(s_temperature_layer, GColorBlack);
@@ -47,6 +82,7 @@ void setup_weather(Layer *root) {
 void teardown_weather() {
   text_layer_destroy(s_temperature_layer);
 #ifdef PBL_COLOR
+  layer_destroy(s_temperature_scale);
 #else
   gbitmap_destroy(s_conditions_bitmap);
   bitmap_layer_destroy(s_conditions_layer);
@@ -105,6 +141,9 @@ void update_weather_temperature_display() {
     }
   }
   text_layer_set_text(s_temperature_layer, temperature_buffer);
+#ifdef PBL_COLOR
+  layer_mark_dirty(s_temperature_scale);
+#endif
 }
 
 void update_weather() {
@@ -122,3 +161,26 @@ void update_weather() {
   }
 #endif
 }
+
+#ifdef PBL_COLOR
+static void temp_scale_update_proc(Layer *layer, GContext *ctx) {
+  int temp_scale = (s_temperature + 4) * 144 / 29;
+  if (temp_scale < 0) {
+    temp_scale = 0;
+  }
+  if (temp_scale > 143) {
+    temp_scale = 143;
+  }
+  for (int i = 0; i < temp_scale; i++) {
+    int line_width = temp_width[i];
+    if (line_width > 0) {
+      int colour_index = i / 11;
+      if (colour_index > 12) {
+        colour_index = 12;
+      }
+      graphics_context_set_stroke_color(ctx, temp_colour[colour_index]);
+      graphics_draw_line(ctx, GPoint(0, 143-i), GPoint(line_width, 143-i));
+    }
+  }
+}
+#endif
